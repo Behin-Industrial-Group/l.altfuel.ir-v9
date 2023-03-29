1d9c0fb9ca6de86769bb2022d47b9a0868611974
mkhodroo <mkhodroo@yahoo.com>
2023-03-29 13:16:35 +0430

    Update irngv.php

--- routes/irngv.php
+++ routes/irngv.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Http\Controllers\IrngvPollAnswerController;
 use App\Http\Controllers\IrngvUsersInfoController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::prefix('/irngv/')->group(function(){
     Route::get('poll/{link}', [IrngvUsersInfoController::class, 'show_poll']);
+    Route::post('register-answer', [IrngvPollAnswerController::class, 'register_answer'])->name('register-poll-answers');
 });
