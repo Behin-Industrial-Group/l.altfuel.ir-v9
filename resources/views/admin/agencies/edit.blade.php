ea354853e281b1eed69e847e1b9559d19185f9f5
mkhodroo <mkhodroo@yahoo.com>
2023-06-13 11:38:44 +0330

    ویرایش تب های مدال ویرایش مرکز

--- resources/views/admin/agencies/edit.blade.php
+++ resources/views/admin/agencies/edit.blade.php
@@ -1,11 +1,14 @@
 @php
    use App\CustomClasses\Access;
 @endphp
-<div class="nav-tabs-custom">
-    <ul class="nav nav-tabs">
-        <li class="active"><a href="#info" data-toggle="tab">مشخصات صنفی</a></li>
-        <li><a href="#fin-info" data-toggle="tab">اطلاعات مالی</a></li>
-    </ul>
+<div class="card card-info">
+    <div class="card-header">
+        <ul class="nav nav-tabs">
+            <li class="nav-item active"><a href="#info" class="nav-link active" data-toggle="tab">مشخصات صنفی</a></li>
+            <li class="nav-item "><a href="#fin-info" class="nav-link " data-toggle="tab">اطلاعات مالی</a></li>
+        </ul>
+    </div>
+    
     <div class="tab-content">
         <div class="active tab-pane" id="info">
             @include('admin.agencies.agency-tab', [ 'agency' => $agency, 'agency_table' => $agency_config['table'] ])
