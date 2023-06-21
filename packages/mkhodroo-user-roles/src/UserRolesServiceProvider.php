0c0875da6e32c6d15d68bb1a82d2f8df835eca09
mkhodroo <mkhodroo@yahoo.com>
2023-06-21 11:56:37 +0330

    user role migrations

--- packages/mkhodroo-user-roles/src/UserRolesServiceProvider.php
+++ packages/mkhodroo-user-roles/src/UserRolesServiceProvider.php
@@ -11,6 +11,9 @@ class UserRolesServiceProvider extends ServiceProvider
     }
 
     public function boot() {
+        $this->publishes([
+            __DIR__ . '/migrations' => database_path('migrations'),
+        ]);
         $this->loadMigrationsFrom(__DIR__ . "/migrations");
         $this->loadRoutesFrom(__DIR__. '/routes.php');;
         $this->loadViewsFrom(__DIR__. '/Views', 'URPackageView');
