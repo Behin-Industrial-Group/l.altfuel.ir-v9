694a1ceeed399f48cfb04eaf3a6c1f96fafbc1ca
mkhodroo <mkhodroo@yahoo.com>
2023-06-28 12:41:39 +0330

    Update add-comment-form.blade.php

--- packages/altfuel-ticket/src/Views/partial-view/add-comment-form.blade.php
+++ packages/altfuel-ticket/src/Views/partial-view/add-comment-form.blade.php
@@ -17,19 +17,19 @@
                 <button class="btn btn-success" id="play-btn" style="display: none">پخش</button>
                 <textarea name="text" id="" class="form-control" rows="4" placeholder="متن پیام"></textarea>
                     <div class="input-group-append">
-                        <div class="input-group-text" id="voice-input" style="cursor: pointer; background:rgb(207, 1, 1); color:white; text-align: center">
+                        {{-- <div class="input-group-text" id="voice-input" style="cursor: pointer; background:rgb(207, 1, 1); color:white; text-align: center">
                             <span class="fa fa-microphone" ></span><br>
                             <span style="writing-mode: vertical-lr;">
                                 نگه دارید
                             </span>
-                        </div>
+                        </div> --}}
                     </div>
                 </div>
                 پیوست: فایل های مجاز {{ json_encode(config('ATConfig.attachment-file-types-translate')) }}
                 <input type="file" class="filepond" name="file">
             </div>
         </div>
-        <div class="row">
+        <div class="row" onclick="submit()">
             <button class="btn btn-success col-sm-12 float-left" id="submit-btn">ثبت</button>
         </div>
     </div>
@@ -42,117 +42,145 @@
 
 <script type="text/javascript">
     // courtesy https://medium.com/@bryanjenningz/how-to-record-and-play-audio-in-javascript-faa1b2b3e49b
-    var recordAudio = () => {
-        audio = '';
-      return new Promise(async resolve => {
-        const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-        const mediaRecorder = new MediaRecorder(stream);
-        const audioChunks = [];
+    // var recordAudio = () => {
+    //     audio = '';
+    //   return new Promise(async resolve => {
+    //     const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    //     const mediaRecorder = new MediaRecorder(stream);
+    //     const audioChunks = [];
 
-        mediaRecorder.addEventListener("dataavailable", event => {
-          audioChunks.push(event.data);
-        });
+    //     mediaRecorder.addEventListener("dataavailable", event => {
+    //       audioChunks.push(event.data);
+    //     });
 
-        const start = () => mediaRecorder.start();
+    //     const start = () => mediaRecorder.start();
 
-        const stop = () =>
-          new Promise(resolve => {
-            mediaRecorder.addEventListener("stop", () => {
-              const audioBlob = new Blob(audioChunks);
-              const audioUrl = URL.createObjectURL(audioBlob);
-              const audio = new Audio(audioUrl);
-              const play = () => audio.play();
-              resolve({ audioBlob, audioUrl, play });
-            });
+    //     const stop = () =>
+    //       new Promise(resolve => {
+    //         mediaRecorder.addEventListener("stop", () => {
+    //           const audioBlob = new Blob(audioChunks);
+    //           const audioUrl = URL.createObjectURL(audioBlob);
+    //           const audio = new Audio(audioUrl);
+    //           const play = () => audio.play();
+    //           resolve({ audioBlob, audioUrl, play });
+    //         });
 
-            mediaRecorder.stop();
-          });
+    //         mediaRecorder.stop();
+    //       });
 
-        resolve({ start, stop });
-      });
-    }
+    //     resolve({ start, stop });
+    //   });
+    // }
 
-    /* simple timeout */
-    var sleep = time => new Promise(resolve => setTimeout(resolve, time));
+    // /* simple timeout */
+    // var sleep = time => new Promise(resolve => setTimeout(resolve, time));
 
-    /* init */
-    (async () => {
-        const btn = $('#voice-input')[0];
-        const playBtn = $('#play-btn');
-        const recorder = await recordAudio();
-        let audio; // filled in end cb
-        const submitBtn = $('#submit-btn')
+    // /* init */
+    // (async () => {
+    //     const btn = $('#voice-input')[0];
+    //     const playBtn = $('#play-btn');
+    //     const recorder = await recordAudio();
+    //     let audio; // filled in end cb
+    //     const submitBtn = $('#submit-btn')
         
 
 
-        const recStart = e => {
-            console.log('start');
-            recorder.start();
-            btn.initialValue = btn.innerHTML;
-            btn.innerHTML = "درحال ضبط...";
-        }
-        const recEnd = async e => {
-            console.log('end');
-            btn.innerHTML = btn.initialValue;
-            audio = await recorder.stop();
-            showPlayAudioBtn();
-            // audio.play();
-            // uploadAudio(audio.audioBlob);
-        }
+    //     const recStart = e => {
+    //         console.log('start');
+    //         recorder.start();
+    //         btn.initialValue = btn.innerHTML;
+    //         btn.innerHTML = "درحال ضبط...";
+    //     }
+    //     const recEnd = async e => {
+    //         console.log('end');
+    //         btn.innerHTML = btn.initialValue;
+    //         audio = await recorder.stop();
+    //         showPlayAudioBtn();
+    //         // audio.play();
+    //         // uploadAudio(audio.audioBlob);
+    //     }
 
-        const recPlay = e => {
-            console.log('play');
-            audio.play();
-        }
+    //     const recPlay = e => {
+    //         console.log('play');
+    //         audio.play();
+    //     }
 
-        const showPlayAudioBtn = e => {
-            console.log('show');
-            playBtn.show()
-        }
+    //     const showPlayAudioBtn = e => {
+    //         console.log('show');
+    //         playBtn.show()
+    //     }
 
 
-        const uploadAudio = a => {
-            // if (a.size > (10 * Math.pow(1024, 2))) {
-            //     document.body.innerHTML += "Too big; could not upload";
-            //     return;
-            // }
-            const f = new FormData($('#{{ $form_id ?? "comment-form" }}')[0]);
+    //     const uploadAudio = a => {
+    //         // if (a.size > (10 * Math.pow(1024, 2))) {
+    //         //     document.body.innerHTML += "Too big; could not upload";
+    //         //     return;
+    //         // }
+    //         // const f = new FormData($('#{{ $form_id ?? "comment-form" }}')[0]);
 
-            if(audio){
-                a = audio.audioBlob;
-                f.append("payload", a);
-            }
-            console.log('send');
-            // f.append("nonce", window.nonce);
-            send_ajax_formdata_request(
-                "{{ route('ATRoutes.store') }}",
-                f,
-                function(response){
-                    ticket = response.ticket;
-                    show_message(response.message)
-                    console.log(response);
-                    if(typeof(show_comment_modal) === "function"){
-                        show_comment_modal(ticket.id, ticket.title, ticket.user_id)
-                    }else{
-                        window.location = "{{ route('ATRoutes.show.listForm') }}"
-                    }
-                },
-                function(data){
-                    show_error(data);
-                    console.log(data);
+    //         // if(audio){
+    //         //     a = audio.audioBlob;
+    //         //     f.append("payload", a);
+    //         // }
+    //         // console.log('send');
+    //         // // f.append("nonce", window.nonce);
+    //         // send_ajax_formdata_request(
+    //         //     "{{ route('ATRoutes.store') }}",
+    //         //     f,
+    //         //     function(response){
+    //         //         ticket = response.ticket;
+    //         //         show_message(response.message)
+    //         //         console.log(response);
+    //         //         if(typeof(show_comment_modal) === "function"){
+    //         //             show_comment_modal(ticket.id, ticket.title, ticket.user_id)
+    //         //         }else{
+    //         //             // window.location = "{{ route('ATRoutes.show.listForm') }}"
+    //         //         }
+    //         //     },
+    //         //     function(data){
+    //         //         show_error(data);
+    //         //         console.log(data);
+    //         //     }
+    //         // )
+    //     }
+
+
+    //     btn.addEventListener("mousedown", recStart);
+    //     btn.addEventListener("touchstart", recStart);
+    //     btn.addEventListener("mouseup", recEnd);
+    //     btn.addEventListener("touchend", recEnd);
+    //     playBtn.on('click', recPlay);
+    //     // submitBtn.on('click', uploadAudio);
+    //     // playBtn.addEventListener("mousedown", recPlay);
+    //     // playBtn.addEventListener("touchstart", recPlay);
+    // })();
+
+    function submit(){
+        const f = new FormData($('#{{ $form_id ?? "comment-form" }}')[0]);
+
+        // if(audio){
+        //     a = audio.audioBlob;
+        //     f.append("payload", a);
+        // }
+        console.log('send1');
+        // f.append("nonce", window.nonce);
+        send_ajax_formdata_request(
+            "{{ route('ATRoutes.store') }}",
+            f,
+            function(response){
+                ticket = response.ticket;
+                show_message(response.message)
+                console.log(response);
+                if(typeof(show_comment_modal) === "function"){
+                    show_comment_modal(ticket.id, ticket.title, ticket.user_id)
+                }else{
+                    // window.location = "{{ route('ATRoutes.show.listForm') }}"
                 }
-            )
-        }
-
-
-        btn.addEventListener("mousedown", recStart);
-        btn.addEventListener("touchstart", recStart);
-        btn.addEventListener("mouseup", recEnd);
-        btn.addEventListener("touchend", recEnd);
-        playBtn.on('click', recPlay);
-        submitBtn.on('click', uploadAudio);
-        // playBtn.addEventListener("mousedown", recPlay);
-        // playBtn.addEventListener("touchstart", recPlay);
-    })();
-    
+            },
+            function(data){
+                show_error(data);
+                console.log(data);
+            }
+        )
+    }
 </script>  
