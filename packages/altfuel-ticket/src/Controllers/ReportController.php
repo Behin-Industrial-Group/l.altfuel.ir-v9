cb71b06b7ebbd7e6cdb08b898112aaf00b1b7036
mkhodroo <mkhodroo@yahoo.com>
2023-07-31 14:15:10 +0330

    Update ReportController.php

--- packages/altfuel-ticket/src/Controllers/ReportController.php
+++ packages/altfuel-ticket/src/Controllers/ReportController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Mkhodroo\AltfuelTicket\Models\Ticket;
 use Mkhodroo\AltfuelTicket\Models\TicketCatagory;
 
@@ -21,8 +22,11 @@ class ReportController extends Controller
     }
 
     function statusInEachCatagory() {
+        Log::info(Carbon::now()->subDays(7)->toDateString());
+        $end = Carbon::createFromFormat('Y-m-d', Carbon::now()->toDateString());
+        $start = Carbon::createFromFormat('Y-m-d', Carbon::now()->subDays(8)->toDateString());
         return response()->json([
-            'labels' => Ticket::whereBetween('created_at', [Carbon::now(), Carbon::now()->subDays(7)])->select('cat_id')->groupBy('cat_id')->get()->each(function($row){
+            'labels' => Ticket::whereBetween('created_at', [$start, $end])->select('cat_id')->groupBy('cat_id')->get()->each(function($row){
                 $row->catagory = $row->catagory()['name'];
                 $row->count = $row->whereCat_id($row->catagory()['id'])->select(DB::raw('count(*) as total'), 'status')->groupBy('status')->get();
             })
