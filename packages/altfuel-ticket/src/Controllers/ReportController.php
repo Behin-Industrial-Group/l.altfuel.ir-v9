304b102d59695490b28ad1f7c8e3813cb67a58e2
mkhodroo <mkhodroo@yahoo.com>
2023-07-31 13:59:57 +0330

    Update ReportController.php

--- packages/altfuel-ticket/src/Controllers/ReportController.php
+++ packages/altfuel-ticket/src/Controllers/ReportController.php
@@ -3,6 +3,7 @@
 namespace Mkhodroo\AltfuelTicket\Controllers;
 
 use App\Http\Controllers\Controller;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Mkhodroo\AltfuelTicket\Models\Ticket;
@@ -21,7 +22,7 @@ class ReportController extends Controller
 
     function statusInEachCatagory() {
         return response()->json([
-            'labels' => Ticket::select('cat_id')->groupBy('cat_id')->get()->each(function($row){
+            'labels' => Ticket::whereBetween('created_at', [Carbon::now(), Carbon::now()->subDays(7)])->select('cat_id')->groupBy('cat_id')->get()->each(function($row){
                 $row->catagory = $row->catagory()['name'];
                 $row->count = $row->whereCat_id($row->catagory()['id'])->select(DB::raw('count(*) as total'), 'status')->groupBy('status')->get();
             })
