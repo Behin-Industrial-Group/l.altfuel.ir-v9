aded6363fbaf76c16e9dc979c56ef6b92ac96d49
mkhodroo <mkhodroo@yahoo.com>
2023-08-22 01:48:08 +0330

    Update test.blade.php

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 {{-- {{ $_SERVER['REMOTE_ADDR'] }} --}}
-{{ env('PM_SERVER') }}
+{{-- {{ env('PM_SERVER') }} --}}
     <iframe src="{{ $src }}" frameborder="0"
         style="width: 100%; height: 85vh"
         id="iframeId"
