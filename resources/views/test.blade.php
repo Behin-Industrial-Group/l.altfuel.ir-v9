95172660eca624c8ef5b0385560da7727b2919f3
mkhodroo <mkhodroo@yahoo.com>
2023-08-23 12:18:35 +0330

    Update test.blade.php

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -6,10 +6,13 @@
     @isset($error)
         <div class="alert alert-danger">{{$error}}</div>
     @endisset
-    <iframe src="{{ $src ?? '' }}" frameborder="0"
+    <iframe src="" frameborder="0"
         style="width: 100%; height: 85vh"
         id="iframeId"
     ></iframe>
 @endsection
+<script>
+    $('iframe').attr('src', '{{$src}}');
+</script>
 
 
