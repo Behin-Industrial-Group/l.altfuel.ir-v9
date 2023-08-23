6a8485ce9aeed5bf6ef66d19bcb86aa470e85b33
mkhodroo <mkhodroo@yahoo.com>
2023-08-23 12:23:50 +0330

    Update test.blade.php

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -3,20 +3,13 @@
 @section('content')
 {{-- {{ $_SERVER['REMOTE_ADDR'] }} --}}
 {{-- {{ env('PM_SERVER') }} --}}
-{{ $pass }}
     @isset($error)
         <div class="alert alert-danger">{{$error}}</div>
     @endisset
-    <iframe src="" frameborder="0"
+    <iframe src="{{ $src ?? '' }}" frameborder="0"
         style="width: 100%; height: 85vh"
         id="iframeId"
     ></iframe>
 @endsection
-@section('script')
-    <script>
-        $('iframe').attr('src', '{{$src}}');
-    </script>
-@endsection
-
 
 
