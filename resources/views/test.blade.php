1af6032750bb2d83657922570e59bb180f149e43
mkhodroo <mkhodroo@yahoo.com>
2023-09-05 09:21:07 +0330

    Update test.blade.php

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -3,7 +3,7 @@
         <div class="alert alert-danger">{{$error}}</div>
     @endisset
     <iframe src="{{ $src ?? '' }}" frameborder="0"
-        style="width: 100%; height: 85vh"
+        style="width: 100%; height: 100vh"
         id="iframeId"
     ></iframe>
 
