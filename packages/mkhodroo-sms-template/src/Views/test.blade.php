bd5e9e29ba54598b8617a34529e6fc80fb90da07
mkhodroo <mkhodroo@yahoo.com>
2023-10-12 19:37:08 +0330

    Update test.blade.php

--- packages/mkhodroo-sms-template/src/Views/test.blade.php
+++ packages/mkhodroo-sms-template/src/Views/test.blade.php
@@ -1,1 +1,5 @@
-تست کاربر {{$params['user_name'] ?? ''}}
+مرکز: {{$params['agency_name'] ?? ''}}
+شما یک اخطاریه از اتحادیه کشوری سوختهای جایگزین و خدمات وابسته دارید.
+لطفا ظرف مدت: {{$params['warning_deadline'] ?? ''}} روز به مامور رسیدگی به این اخطاریه مراجعه کنید.
+محل رسیدگی به اخطاریه: {{ $params['warning_handling_place'] ?? '' }}
+تلفن: {{ $params['warning_handling_phone'] }}
