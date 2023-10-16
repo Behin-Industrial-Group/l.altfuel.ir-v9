e87514dc25b59082cb05c7977def8c0102124c72
mkhodroo <mkhodroo@yahoo.com>
2023-10-16 18:31:33 +0330

    Update DynaFormController.php

--- packages/mkhodroo-process-maker/src/Controllers/DynaFormController.php
+++ packages/mkhodroo-process-maker/src/Controllers/DynaFormController.php
@@ -104,8 +104,11 @@ class DynaFormController extends Controller
                         echo  "</div>";
                     }
                     if ($field->type == "datetime") {
-                        $date = new SDate();
-                        $field_value = $date->toGrDate($field_value);
+                        if($field_value){
+                            $date = new SDate();
+                            $field_value = $date->toGrDate($field_value);
+                        }
+                        
                         echo  "<div class='col-sm-$field->colSpan'>";
                         echo  "$field->label: <input type='text' name='$field->name' class='form-control persian-date' value='$field_value' $field_required $field_mode>";
                         echo  "</div>";
