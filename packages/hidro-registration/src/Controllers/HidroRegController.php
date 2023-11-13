eb456dca230c8869dce9df21672ee1b4dced501d
mkhodroo <mkhodroo@yahoo.com>
2023-11-13 14:07:57 +0330

    Update HidroRegController.php

--- packages/hidro-registration/src/Controllers/HidroRegController.php
+++ packages/hidro-registration/src/Controllers/HidroRegController.php
@@ -33,16 +33,16 @@ class HidroRegController extends Controller
                     'debt' => '50000000',
                     'Details' => "اضافه شده از اکسل سیمفا"
                 ]);
-                // HidroModel::create([
-                //     'simfaCode' => $row[0],
-                //     'Name' => $row[1],
-                //     'Address' => $row[2],
-                //     'PostalCode' => $row[3],
-                //     'legalNationalId' => $row[4],
-                //     'Tel' => $row[5],
-                //     'debt' => '50000000',
-                //     'Details' => "اضافه شده از اکسل سیمفا"
-                // ]);
+                HidroModel::create([
+                    'simfaCode' => $row[0],
+                    'Name' => $row[1],
+                    'Address' => $row[2],
+                    'PostalCode' => $row[3],
+                    'legalNationalId' => $row[4],
+                    'Tel' => $row[5],
+                    'debt' => '50000000',
+                    'Details' => "اضافه شده از اکسل سیمفا"
+                ]);
             }
         }
     }
