3abb1d2b0168318bdeda4fa65c90f38f28983162
mkhodroo <mkhodroo@yahoo.com>
2023-11-14 14:27:59 +0330

    Update routes.php

--- packages/hidro-registration/src/routes.php
+++ packages/hidro-registration/src/routes.php
@@ -4,7 +4,7 @@ use Hidro\Registration\Controllers\HidroRegController;
 use Illuminate\Support\Facades\Route;
 use Mkhodroo\Voip\Controllers\VoipController;
 
-Route::name('hidroReg.')->prefix('hidro-registration')->middleware(['web'])->group(function(){
+Route::name('hidroReg.')->prefix('hidro-registraiton')->middleware(['web'])->group(function(){
     // Route::get('upload-excel', [HidroRegController::class, 'uploadExcel'])->name('uploadExcel');
     Route::get('find-form', [HidroRegController::class, 'findForm'])->name('findForm');
     Route::post('compelete-info-form', [HidroRegController::class, 'compeleteInfoForm'])->name('compeleteInfoForm');
