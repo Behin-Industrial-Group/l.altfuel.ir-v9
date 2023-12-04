e73adda27562ff8d3c1de8b824441e10d77f603f
mkhodroo <mkhodroo@yahoo.com>
2023-12-04 11:48:37 +0330

    Update RGenCode.php

--- app/Repository/RGenCode.php
+++ app/Repository/RGenCode.php
@@ -50,7 +50,7 @@ class RGenCode
 
     public function Hidro()
     {
-        return $this->province_ids;
+        return $this->parent_ids;
         $last_code = $this->GetLastHidroCode();
 
         if($last_code !== null){
