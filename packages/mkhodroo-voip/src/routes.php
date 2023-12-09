49ff37a46000335e33e6f7baf717c8e70ba61394
mkhodroo <mkhodroo@yahoo.com>
2023-12-09 13:08:17 +0330

    Update routes.php

--- packages/mkhodroo-voip/src/routes.php
+++ packages/mkhodroo-voip/src/routes.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Mkhodroo\Voip\Controllers\FirstOnlineTimeController;
 use Mkhodroo\Voip\Controllers\VoipController;
 
 Route::name('voip.')->prefix('voip')->middleware(['web', 'auth', 'access'])->group(function(){
@@ -8,6 +9,7 @@ Route::name('voip.')->prefix('voip')->middleware(['web', 'auth', 'access'])->gro
     Route::get('get-peer-poll-info/{queue_num}', [VoipController::class, 'getPeerPollInfo'])->name('getPeerPollInfo');
     Route::get('get-call-report/{ext_num?}', [VoipController::class, 'getCallReport'])->name('getCallReport');
     Route::post('dl-voice', [VoipController::class, 'dlVoice'])->name('dlVoice');
+    Route::get('first-online-avg', [FirstOnlineTimeController::class, 'avg'])->name('firstOnlineAvg');
 
     Route::get('test', function(){
         return view('VoipViews::webphone.webphone');
