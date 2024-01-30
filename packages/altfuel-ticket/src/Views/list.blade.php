0dec3da45c13dfa87e8cc288a53fa5a71bb07ea0
mkhodroo <mkhodroo@yahoo.com>
2024-01-31 01:10:02 +0330

    Update list.blade.php

--- packages/altfuel-ticket/src/Views/list.blade.php
+++ packages/altfuel-ticket/src/Views/list.blade.php
@@ -58,7 +58,22 @@
                 }},
                 {data: 'user'},
                 {data: 'catagory.name'},
-                {data: 'status'},
+                {data: 'status', render: function(data){
+                    if(data == "{{config('ATConfig.status.new')}}"){
+                        return '1-' + data
+                    }
+                    else if(data == "{{config('ATConfig.status.in_progress')}}"){
+                        return '2-' + data
+                    }
+                    else if(data == "{{config('ATConfig.status.answered')}}"){
+                        return '3-' + data
+                    }
+                    else if(data == "{{config('ATConfig.status.closed')}}"){
+                        return '4-' + data
+                    }else{
+                        return data
+                    }
+                }},
                 {data: 'updated_at'}
             ],
             null,
