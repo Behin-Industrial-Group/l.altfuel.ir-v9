702c0596dcb102b333059188943d1e81e8d4c659
mkhodroo <mkhodroo@yahoo.com>
2024-02-28 12:18:20 +0330

    Update zarinpal.php

--- config/zarinpal.php
+++ config/zarinpal.php
@@ -1,7 +1,7 @@
 <?php 
 
 return [
-    'merchantId' => '6a6243dd-534e-4ea5-9b42-0b2cf1da4a09',
+    'merchantId' => 'cdc4e79d-ff6e-4684-ab94-8d74049ef5c9',
     'pay_url' => 'https://www.zarinpal.com/pg/StartPay/',
     'payment_verification_url' => 'https://www.zarinpal.com/pg/services/WebGate/wsdl'
 ];
