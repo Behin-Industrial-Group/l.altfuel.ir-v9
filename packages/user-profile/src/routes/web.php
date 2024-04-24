8a22c5ed571f2303976cdff6e4b3a225dd501582
Amirali Hosseini <amiralihosseini216@yahoo.com>
2024-04-24 12:26:25 +0330

    user profile national_id store

--- packages/user-profile/src/routes/web.php
+++ packages/user-profile/src/routes/web.php
@@ -2,10 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use UserProfile\Controllers\ChangePasswordController;
+use UserProfile\Controllers\NationalIdController;
 use UserProfile\Controllers\UserProfileController;
 
 Route::name('user-profile.')->prefix('user-profile')->middleware(['web','auth'])->group(function(){
     Route::get('/', [UserProfileController::class, 'index'])->name('profile');
     Route::get('/change-password', [ChangePasswordController::class, 'edit'])->name('change-password');
+    Route::post('', [NationalIdController::class, 'store'])->name('storeNationalId');
     Route::put('/', [ChangePasswordController::class, 'update'])->name('update-password');
 });
--- packages/user-profile/src/views/index.blade.php
+++ packages/user-profile/src/views/index.blade.php
@@ -13,7 +13,40 @@
             <div class="col-sm-2">شماره موبایل :</div>
             <div class="col-sm-10">{{ $user->email }}</div>
             <hr>
-            <a href="{{ route('user-profile.change-password') }}" class="col-sm-4 btn btn-primary btn-sm" >تغییر رمز عبور</a>
+            @if ($userProfile?->national_id)
+                <div class="col-sm-2">کد ملی :</div>
+                <div class="col-sm-10">{{ $userProfile->national_id }}</div>
+            @else
+            <form action="javascript:void(0)" method="POST" id="store-national-id-form">
+                @csrf
+                <section class="col-sm-12">
+                    <div class="form-group">
+                        <div class="col-sm-2">کد ملی :</div>
+                        <input type="text" class="form-control form-control-sm my-2 col-sm-6" name="national_id" id="national_id">
+                        <button class="btn btn-primary btn-sm col-sm-4" onclick="store_national_id()">ثبت</button>
+                    </div>
+                </section>
+            </form>
+            @endif
+            <hr>
+            <a href="{{ route('user-profile.change-password') }}" class="col-sm-12 btn btn-primary btn-sm" >تغییر رمز عبور</a>
         </div>
     </div>
+@endsection
+
+@section('script')
+    <script>
+        function store_national_id(){
+            var form = $('#store-national-id-form')[0];
+            var data = new FormData(form);
+            send_ajax_formdata_request(
+                "{{ route('user-profile.storeNationalId') }}",
+                data,
+                function(response){
+                    show_message("{{trans('ok')}}")
+                    location.reload()
+                }
+            )
+        }
+    </script>
 @endsection
