cc1ef2dc4556a79c8b47698e7b7246a7c247eb83
mkhodroo <mkhodroo@yahoo.com>
2024-04-24 20:24:57 +0330

    Update index.blade.php

--- packages/user-profile/src/views/index.blade.php
+++ packages/user-profile/src/views/index.blade.php
@@ -7,29 +7,36 @@
 @section('content')
     <div class="p-4">
         <div class="row m-10">
-            <div class="col-sm-2">نام کاربری :</div>
-            <div class="col-sm-10">{{ $user->display_name }}</div>
+            <div class="col-sm-6 row">
+                <div class="col-sm-4">نام کاربری :</div>
+                <div class="col-sm-6">{{ $user->display_name }}</div>
+                <hr>
+                <div class="col-sm-4">شماره موبایل :</div>
+                <div class="col-sm-6">{{ $user->email }}</div>
+                <hr>
+                @if ($userProfile?->national_id)
+                    <div class="col-sm-4">کد ملی :</div>
+                    <div class="col-sm-6">{{ $userProfile->national_id }}</div>
+                @else
+                <form action="javascript:void(0)" class="row" id="store-national-id-form">
+                    @csrf
+                    <div class="col-sm-4">کد ملی :</div>
+                    <input type="text" class="form-control col-sm-6" name="national_id" id="national_id">
+                    <button class="btn btn-primary btn-sm col-sm-2" onclick="store_national_id()">ثبت</button>
+                </form>
+                @endif
+                <div class="row">
+
+                </div>
+            </div>
+            <div class="col-sm-3"></div>
+            <div class="col-sm-3">
+                <a href="{{ route('user-profile.change-password') }}" class="col-sm-10 btn btn-danger btn-sm" >تغییر رمز عبور</a>
+            </div>
+            
+            
+            
             <hr>
-            <div class="col-sm-2">شماره موبایل :</div>
-            <div class="col-sm-10">{{ $user->email }}</div>
-            <hr>
-            @if ($userProfile?->national_id)
-                <div class="col-sm-2">کد ملی :</div>
-                <div class="col-sm-10">{{ $userProfile->national_id }}</div>
-            @else
-            <form action="javascript:void(0)" method="POST" id="store-national-id-form">
-                @csrf
-                <section class="col-sm-12">
-                    <div class="form-group">
-                        <div class="col-sm-2">کد ملی :</div>
-                        <input type="text" class="form-control form-control-sm my-2 col-sm-6" name="national_id" id="national_id">
-                        <button class="btn btn-primary btn-sm col-sm-4" onclick="store_national_id()">ثبت</button>
-                    </div>
-                </section>
-            </form>
-            @endif
-            <hr>
-            <a href="{{ route('user-profile.change-password') }}" class="col-sm-12 btn btn-primary btn-sm" >تغییر رمز عبور</a>
         </div>
     </div>
 @endsection
