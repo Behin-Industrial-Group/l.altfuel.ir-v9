ec5ecbd1e5ddb476e2950d75369c36334cde8545
Amirali Hosseini <amiralihosseini216@yahoo.com>
2024-04-28 11:16:33 +0330

    view category column in method access

--- packages/mkhodroo-user-roles/src/Views/roles/edit.blade.php
+++ packages/mkhodroo-user-roles/src/Views/roles/edit.blade.php
@@ -2,22 +2,35 @@
     {{ $role->name }}
     <form action="javascript:void()" id="method-form">
         @csrf
-        <input type="text" name="role_id" id="" value="{{$role->id}}">
-    @foreach ($methods as $method)
-            <input type="checkbox" name="{{$method->id}}" id="" {{ $method->access ? 'checked' : '' }}>{{$method->name}}
-            <br>
-    @endforeach
-    </form >
+        <input type="text" name="role_id" id="" value="{{ $role->id }}">
+        <table class="table table-stripped">
+            <thead>
+                <tr>
+                    <th>نام</th>
+                    <th>دسته بندی</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($methods as $method)
+                    <tr>
+                        <td><input type="checkbox" name="{{ $method->id }}" id="" {{ $method->access ? 'checked' : '' }}>{{ $method->name }}</td>
+                        <td>{{ $method->category }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+    </form>
     <button onclick="submit()">submit</button>
 
 
 </div>
 <script>
-    function submit(){
+    function submit() {
         send_ajax_request(
             "{{ route('role.edit') }}",
             $('#method-form').serialize(),
-            function(data){
+            function(data) {
                 console.log(data);
             }
         )
