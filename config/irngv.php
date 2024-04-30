326c33017f8d10d8ecb1562f537c893db3f4d222
mkhodroo <mkhodroo@yahoo.com>
2024-04-30 17:25:48 +0330

    Update irngv.php

--- config/irngv.php
+++ config/irngv.php
@@ -3,5 +3,9 @@
 
 return [
     'irngv-poll-link' => 'https://l.altfuel.ir/irngv/poll/',
-    'valid_reg_type' => [ 'niopdc', 'simfa', 'none' ]
+    'valid_reg_type' => [ 
+        1, //for none 
+        2, // for niopdc
+        3  // for simfa
+    ]
 ];
