ce822fb9ee53640e38f9e05aa73489b62fa0a71a
Amirali Hosseini <amiralihosseini216@yahoo.com>
2024-05-08 15:14:59 +0330

    ai assistant package

--- packages/behin-ai-assistant/src/routes/api.php
+++ packages/behin-ai-assistant/src/routes/api.php
@@ -6,6 +6,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::name('aiAssistant.')->prefix('ai-assistant')->group(function(){
-    Route::get('/text-to-speech', [TextToSpeechController::class, 'sendText'])->name('sendText');
+    Route::post('/text-to-speech', [TextToSpeechController::class, 'sendText'])->name('sendText');
     Route::post('/speech-to-text', [SpeechToTextController::class, 'sendSpeech'])->name('sendSpeech');
 });
