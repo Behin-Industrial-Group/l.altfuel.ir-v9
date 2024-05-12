b5d8973f1079d53972161f35fdbe0f08c0aa3833
Amirali Hosseini <amiralihosseini216@yahoo.com>
2024-05-12 15:46:51 +0330

    irngv poll update

--- packages/behin-irngv-poll/src/IrngvPollProvider.php
+++ packages/behin-irngv-poll/src/IrngvPollProvider.php
@@ -23,8 +23,6 @@ class IrngvPollProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->loadMigrationsFrom(__DIR__ . '/migrations');
         $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
-        $this->loadViewsFrom(__DIR__. '/views', 'IrngvPollViews');
     }
 }
--- packages/behin-irngv-poll/src/views/partial-views/user-profile-icon.blade.php
+++
@@ -1,5 +0,0 @@
-<li class="">
-    <a class="" href="{{ route('user-profile.profile') }}">
-        <i class="fa fa-user"></i>
-    </a>
-</li>
