4a9b56aa353f32de3c9f48f6a3c258517317dea7
mkhodroo <info@behin.energy>
2024-06-12 11:08:17 +0330

    Update web.php

--- packages/behin-telegram-bot/src/routes/web.php
+++ packages/behin-telegram-bot/src/routes/web.php
@@ -5,5 +5,5 @@ use Illuminate\Support\Facades\Route;
 use TelegramBot\Controllers\BotController;
 
 Route::name('telegramBot.')->prefix('telegram-bot')->group(function(){
-    Route::get('/chat', [BotController::class, 'chat'])->name('chat');
+    Route::post('/chat', [BotController::class, 'chat'])->name('chat');
 });
