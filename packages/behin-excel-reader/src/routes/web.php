9b742f082531c2a7b1a5df86c7770b5d02cfa38f
behinIndustrialGroup <info@behin.energy>
2024-10-14 13:19:02 +0330

    Update web.php

--- packages/behin-excel-reader/src/routes/web.php
+++ packages/behin-excel-reader/src/routes/web.php
@@ -3,7 +3,7 @@
 use ExcelReader\Controllers\ExcelController;
 use Illuminate\Support\Facades\Route;
 
-Route::name('excelReader.')->prefix('excel')->group(function(){
+Route::name('excelReader.')->prefix('excel')->group(function () {
     Route::get('input', [ExcelController::class, 'input'])->name('input');
     Route::post('excel-reader', [ExcelController::class, 'read'])->name('read');
-    });
+});
