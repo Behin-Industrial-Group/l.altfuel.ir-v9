b8b98740649b855382844678354d98f4721d3f6b
behinIndustrialGroup <info@behin.energy>
2024-10-29 19:29:44 +0330

    Update routes.php

--- packages/behin-complaint/src/routes.php
+++ packages/behin-complaint/src/routes.php
@@ -7,9 +7,6 @@ use Mkhodroo\Cities\Controllers\CityViewController;
 use Mkhodroo\Cities\Controllers\ProvinceController;
 
 Route::name('complaint.')->prefix('complaint')->middleware(['web'])->group(function () {
-    Route::get('', function(){
-        return view('ComplaintViews::emails.complaint_submitted');
-    })->name('create');
     Route::get('create', [ComplaintController::class, 'create'])->name('create');
     Route::post('complaints', [ComplaintController::class, 'store'])->name('store');
 
