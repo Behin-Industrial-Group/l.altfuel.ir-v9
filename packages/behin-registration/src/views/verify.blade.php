958e20cc8cb783039955069310b5579d2196dee4
Amirali Hosseini <amiralihosseini216@yahoo.com>
2024-10-30 10:51:39 +0330

    price in database fix

--- packages/behin-registration/src/views/verify.blade.php
+++ packages/behin-registration/src/views/verify.blade.php
@@ -5,17 +5,17 @@
 @endsection
 @section('content')
     @if ($refId === 0 or $refId === 1)
-        <div class="container mt-5 text-center">
+        <div class="container pt-5 text-center">
             <div class="alert alert-danger">
                 <h4 class="alert-heading">پرداخت ناموفق بود</h4>
                 <p>متاسفانه پرداخت شما انجام نشد. لطفا دوباره تلاش کنید.</p>
                 <hr>
                 <p class="mb-0">در صورت بروز مشکل، با پشتیبانی تماس بگیرید.</p>
             </div>
-            <a href="{{  }}" class="btn btn-primary mt-3">بازگشت به صفحه پرداخت</a>
+            {{-- <a href="{{  }}" class="btn btn-primary mt-3">بازگشت به صفحه پرداخت</a> --}}
         </div>
     @else
-        <div class="container mt-5 text-center">
+        <div class="container pt-5 text-center">
             <div class="alert alert-success">
                 <h4 class="alert-heading">پرداخت با موفقیت انجام شد!</h4>
                 <p>کد رهگیری شما:</p>
@@ -23,7 +23,7 @@
                 <hr>
                 <p class="mb-0">از خرید شما متشکریم.</p>
             </div>
-            <a href="{{ }}" class="btn btn-primary mt-3">بازگشت به صفحه اصلی</a>
+            {{-- <a href="{{ }}" class="btn btn-primary mt-3">بازگشت به صفحه اصلی</a> --}}
         </div>
     @endif
 @endsection
