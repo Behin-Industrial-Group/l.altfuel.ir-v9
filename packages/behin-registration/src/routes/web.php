768549b490c2282ddb3d6c51385da1ff42ae52a6
behinIndustrialGroup <info@behin.energy>
2024-11-23 11:21:29 +0330

    Update web.php

--- packages/behin-registration/src/routes/web.php
+++ packages/behin-registration/src/routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Registration\Controllers\RegisterUserController;
 
 Route::name('azmoon.')->prefix('azmoon')->middleware(['web'])->group(function(){
-    Route::get('/', [RegisterUserController::class, 'showForm'])->name('form');
+    Route::get('/register', [RegisterUserController::class, 'showForm'])->name('form');
     Route::post('/register', [RegisterUserController::class, 'submitForm'])->name('submit');
     Route::get('/verify', [RegisterUserController::class, 'verify'])->name('verify');
 });
