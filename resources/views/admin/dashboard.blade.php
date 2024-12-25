59155aff663f4e87f2809fe637563125cdc41c02
behinIndustrialGroup <info@behin.energy>
2024-12-25 11:45:51 +0330

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -6,6 +6,60 @@
 
 @section('content')
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <div class="row">
+        @if(auth()->user()->access('پذیرش دوره برق خورشیدی'))
+        <div class="col-lg-3 col-6">
+            <div class="small-box bg-info">
+                <div class="inner">
+                    <p>پذیرش دوره برق خورشیدی</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-bag"></i>
+                </div>
+                <a href="{{ route('event-verification.verify-form', 1) }}" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+        @endif
+        @if(auth()->user()->access('پذیرش دوره خودرو برقی'))
+        <div class="col-lg-3 col-6">
+            <div class="small-box bg-info">
+                <div class="inner">
+                    <p>پذیرش دوره خودرو برقی</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-bag"></i>
+                </div>
+                <a href="{{ route('event-verification.verify-form', 2) }}" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+        @endif
+        @if(auth()->user()->access('پذیرش دوره گواهی سلامت'))
+        <div class="col-lg-3 col-6">
+            <div class="small-box bg-info">
+                <div class="inner">
+                    <p>پذیرش دوره گواهی سلامت</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-bag"></i>
+                </div>
+                <a href="{{ route('event-verification.verify-form', 3) }}" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+        @endif
+        @if(auth()->user()->access('پذیرش همایش'))
+        <div class="col-lg-3 col-6">
+            <div class="small-box bg-info">
+                <div class="inner">
+                    <p>پذیرش همایش</p>
+                </div>
+                <div class="icon">
+                    <i class="ion ion-bag"></i>
+                </div>
+                <a href="{{ route('event-verification.verify-form', 4) }}" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+        @endif
+    </div>
 
 
     {{-- تعداد مرخصی ها --}}
