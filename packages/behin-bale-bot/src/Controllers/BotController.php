ec1f279052ebe869adc23a5a68f97c2acc6a7cfa
Amirali Hosseini <amiralihosseini216@yahoo.com>
2025-04-09 15:41:45 +0330

    bale fix

--- packages/behin-bale-bot/src/Controllers/BotController.php
+++ packages/behin-bale-bot/src/Controllers/BotController.php
@@ -68,7 +68,7 @@ class BotController extends Controller
 
     // }
 
-    public function chat()
+    public function handle()
     {
         Log::info("Receive Message");
         $content = file_get_contents('php://input');
--- packages/behin-bale-bot/src/routes/web.php
+++ packages/behin-bale-bot/src/routes/web.php
@@ -4,7 +4,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use BaleBot\Controllers\BotController;
 
-Route::name('baleBot.')->prefix('bale-bot')->group(function(){
-    Route::post('/chat', [BotController::class, 'chat'])->name('chat');
-    Route::post('/callback', [BotController::class, 'handleCallback']);
-});
+
+Route::post('/bale/webhook', [BotController::class, 'handle']);
+Route::post('/bale/callback', [BotController::class, 'handleCallback']);
