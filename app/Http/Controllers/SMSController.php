2608b61c4c4efc8dfabca331a8ba23c69e6cedf8
Amirali Hosseini <amiralihosseini216@yahoo.com>
2025-05-20 10:48:48 +0330

    sms controller fix

--- app/Http/Controllers/SMSController.php
+++ app/Http/Controllers/SMSController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\SmsLog;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 
 class SMSController extends Controller
@@ -23,43 +24,58 @@ class SMSController extends Controller
     }
     public function send($to, $msg)
     {
-        $data = array(
-            'organization' => $this->org,
-            'username' => $this->user,
-            'password' => $this->pass,
-            'method' => 'send',
-            'messages' => array([
-                'sender' => '9820003807',
-                'recipient' => $to,
-                'body' => $msg,
-                'customerId' => 1,
-            ]
-            )
-        );
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $this->url);
-        # Setup request to send json via GET.
-        $payload = json_encode($data);
-        
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, False);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, False);
-    
-        # Return response instead of printing.
-        # Send request.
-        $er = curl_error($ch);
-        if($er)
-            Log::info("send sms curl error: $er ");
-        $result = curl_exec($ch);
-        curl_close($ch);
-        $result = json_decode($result);
-        if(isset($result->data[0]->serverId)){
-            SmsLog::set(Auth::user(), $to, $msg);
-            return 'ok';
+        // $data = array(
+        //     'organization' => $this->org,
+        //     'username' => $this->user,
+        //     'password' => $this->pass,
+        //     'method' => 'send',
+        //     'messages' => array([
+        //         'sender' => '9820003807',
+        //         'recipient' => $to,
+        //         'body' => $msg,
+        //         'customerId' => 1,
+        //     ]
+        //     )
+        // );
+        // $ch = curl_init();
+        // curl_setopt($ch, CURLOPT_URL, $this->url);
+        // # Setup request to send json via GET.
+        // $payload = json_encode($data);
+
+        // curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+        // curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+        // curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        // curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, False);
+        // curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, False);
+
+        // # Return response instead of printing.
+        // # Send request.
+        // $er = curl_error($ch);
+        // if($er)
+        //     Log::info("send sms curl error: $er ");
+        // $result = curl_exec($ch);
+        // curl_close($ch);
+        // $result = json_decode($result);
+        // if(isset($result->data[0]->serverId)){
+        //     SmsLog::set(Auth::user(), $to, $msg);
+        //     return 'ok';
+        // }
+        // return 'not ok';
+
+
+        // $msg += "\n اتحادیه ملی سوختهای جایگزین";
+        $response = Http::withHeaders([
+            'X-API-KEY' => env('SMS_TOKEN'),
+        ])->post('https://iran.altfuel.ir/sms/index.php', [
+            'to' => $to,
+            'message' => $msg
+        ]);
+        Log::info($response);
+        if ($response->successful()) {
+            echo $response->body(); // یا log کن یا ذخیره کن
+        } else {
+            echo "خطا در ارسال SMS";
         }
-        return 'not ok';
     }
 
     public function send_multiple(array $messages)
@@ -75,13 +91,13 @@ class SMSController extends Controller
         curl_setopt($ch, CURLOPT_URL, $this->url);
         # Setup request to send json via GET.
         $payload = json_encode($data);
-        
+
         curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, False);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, False);
-    
+
         # Return response instead of printing.
         # Send request.
         $er = curl_error($ch);
