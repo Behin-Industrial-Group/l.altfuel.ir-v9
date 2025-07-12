d3da1158e73fcba196858a646166969eefb3fa7f
Amirali Hosseini <amiralihosseini216@yahoo.com>
2025-07-12 10:15:07 +0330

    login page backup phone alert

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,54 +1,58 @@
 @extends('layouts.welcome')
 
 @section('content')
-<div class="register-box">
-    <div class="card card-outline card-primary">
-        <div class="card-header text-center">
-            <img src="https://altfuel.ir/fa/public/logo.png" class="col-sm-12" alt="">
-        </div>
-        <div class="card-body">
-            <form action="javascript:void(0)" method="post" id="login-form">
-                @csrf
-                <div class="input-group mb-3">
-                    <input type="text" class="form-control" name="email" placeholder="موبایل">
-                    <div class="input-group-append">
-                        <div class="input-group-text">
-                            <span class="fa fa-phone"></span>
+    <div class="register-box">
+        <div class="card card-outline card-primary">
+            <div class="card-header text-center">
+                <img src="https://altfuel.ir/fa/public/logo.png" class="col-sm-12" alt="">
+            </div>
+            <div class="card-body">
+                {{-- 🔔 اطلاعیه تماس --}}
+                <div class="alert alert-warning text-right" role="alert" style="font-size: 14px;">
+                    در صورتی که موفق به تماس با خطوط اصلی نشدید، لطفاً با شماره پشتیبان <strong>02191012988</strong> تماس
+                    بگیرید.
+                </div>
+                <form action="javascript:void(0)" method="post" id="login-form">
+                    @csrf
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" name="email" placeholder="موبایل">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fa fa-phone"></span>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="input-group mb-3">
-                    <input type="password" class="form-control" name="password" placeholder="رمز عبور">
-                    <div class="input-group-append">
-                        <div class="input-group-text">
-                            <span class="fa fa-lock"></span>
+                    <div class="input-group mb-3">
+                        <input type="password" class="form-control" name="password" placeholder="رمز عبور">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fa fa-lock"></span>
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div class="row">
-                    <div class="col-8">
-                        <div class="icheck-primary">
+                    <div class="row">
+                        <div class="col-8">
+                            <div class="icheck-primary">
+                            </div>
                         </div>
                     </div>
+                </form>
+                <div class="col-12">
+                    <button type="submit" class="btn btn-primary col-sm-12" onclick="submit()">ورود</button>
                 </div>
-            </form>
-            <div class="col-12">
-                <button type="submit" class="btn btn-primary col-sm-12" onclick="submit()">ورود</button>
+                <hr>
+                <div class="center-align" style="text-align: center">
+                    <a href="{{ route('register') }}" class="text-center">صفحه ثبت نام</a>
+                </div>
+                <hr>
+                <div class="center-align" style="text-align: center">
+                    <a href="{{ route('password.request') }}" class="text-center">فراموشی رمز</a>
+                </div>
             </div>
-            <hr>
-            <div class="center-align" style="text-align: center">
-                <a href="{{ route('register') }}" class="text-center">صفحه ثبت نام</a>
-            </div>
-            <hr>
-            <div class="center-align" style="text-align: center">
-                <a href="{{ route('password.request') }}" class="text-center">فراموشی رمز</a>
-            </div>
-        </div>
 
+        </div>
     </div>
-</div>
-
 @endsection
 
 @section('script')
@@ -71,10 +75,10 @@
     </script>
     <script>
         document.getElementById("login-form").addEventListener("keypress", function(event) {
-          if (event.key === "Enter") {
-            event.preventDefault(); // از ارسال فرم به صورت پیش‌فرض جلوگیری می‌کند
-            document.querySelector("button[type='submit']").click(); // دکمه لاگین را شبیه‌سازی می‌کند
-          }
+            if (event.key === "Enter") {
+                event.preventDefault(); // از ارسال فرم به صورت پیش‌فرض جلوگیری می‌کند
+                document.querySelector("button[type='submit']").click(); // دکمه لاگین را شبیه‌سازی می‌کند
+            }
         });
-      </script>
+    </script>
 @endsection
